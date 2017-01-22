d0de4d3d2334036df5120c86e73668cff7a816da
Galaxymilky <niuben2010@yeah.net>
2017-01-22 22:30:07 +0800

    leetcode commit

--- src/main/java/work/utils/FileUtils.java
+++ src/main/java/work/utils/FileUtils.java
@@ -1,11 +1,36 @@
 package work.utils;
 
+import org.apache.commons.codec.digest.DigestUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.*;
+import java.math.BigInteger;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.Provider;
+import java.security.Security;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.commons.codec.digest.DigestUtils;
 
 /**
  * Created by niu_ben on 2016/11/30.
  */
 public class FileUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(FileUtils.class);
 
     public static void main(String[] args) {
         FileUtils fileUtils = new FileUtils();
@@ -22,15 +47,17 @@ public class FileUtils {
          fileUtils.writeFile(xmlStr);
          */
 
+
 //        String s = fileUtils.readFile("C:\\syndata", "20161222152216.xml");
 //        System.out.println(s);
 
-        String path = "ftp://172.18.116.36:21/File";
-        File file = new File(path);
-        System.out.println(file.getPath());
+
+//        String path = "ftp://172.18.116.36:21/File";
+//        File file = new File(path);
+//        System.out.println(file.getPath());
+
 
     }
-
 
 
     /**
@@ -129,5 +156,29 @@ public class FileUtils {
         }
     }
 
-
+    /**
+     * @param file
+     */
+    public static String getFileMd5(File file) throws FileNotFoundException {
+        String value = null;
+        FileInputStream in = new FileInputStream(file);
+        try {
+            MappedByteBuffer byteBuffer = in.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            md5.update(byteBuffer);
+            BigInteger bi = new BigInteger(1, md5.digest());
+            value = bi.toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (null != in) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return value;
+    }
 }
